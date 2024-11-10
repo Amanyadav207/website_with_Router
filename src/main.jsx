0e49720e03ed8eb28a78b4c5c9dc70eede53c712
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-11-10 19:11:40 +0530

    router

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+import User from './Components/User/USer.jsx'
+import Github, { loader } from './Components/Github/Github.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element= {<Layout/>} >
+      <Route path='' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/user/:id' element= {<User/>} />
+      <Route 
+      path='/Github' 
+      loader={loader}
+      element= {<Github/>}
+       />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
