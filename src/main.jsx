123754300ddab09d0538653a245b73569c1d555e
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-11-11 19:55:49 +0530

    import fisex

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
 import Contact from './Components/Contact/Contact.jsx'
-import User from './Components/User/USer.jsx'
+import User from './Components/User/User.jsx'
 import Github, { loader } from './Components/Github/Github.jsx'
 
 const router = createBrowserRouter(
